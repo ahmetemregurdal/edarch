752a4a2907c3dd1d4032588b91778bda1b8855e3
ahmetemregurdal <ahmetemregurdal2008@gmail.com>
2023-06-07 19:59:56 +0300

    it now works

--- archlive/airootfs/usr/local/bin/install.sh
+++ archlive/airootfs/usr/local/bin/install.sh
@@ -37,34 +37,34 @@ BOOT="/dev/$BOOTNAME"
 echo "Input your root partition[ex. nvme0n1p2]"
 read -r ROOTNAME
 ROOT="/dev/$ROOTNAME"
-exho "If you have a swap partition then input it, else input n[ex. nvme0n1p3 or n]"
+echo "If you have a swap partition then input it, else input n[ex. nvme0n1p3 or n]"
 read -r SWAPNAME
 if [[ ${SWAPNAME} != "n" ]]; then
 	SWAP="/dev/$SWAPNAME"
 fi
-mkfs.vfat -F 32 "$BOOT"
-mkfs.ext4 "$ROOT"
+mkfs.vfat -F 32 "$BOOT" >&/dev/null
+mkfs.ext4 "$ROOT" >&/dev/null
 if [[ ${SWAPNAME} != "n" ]]; then
-	mkswap "$SWAP"
+	mkswap "$SWAP" >&/dev/null
 fi
 mount "$ROOT" /mnt
 mount --mkdir "$BOOT" /mnt/boot
 if [[ ${SWAPNAME} != "n" ]]; then
 	swapon "$SWAP"
 fi
-pacstrap -K /mnt base linux linux-firmware networkmanager vim nano man-db e2fsprogs ntfs-3g dosfstools grub
+echo "bootstraping"
+pacstrap -K /mnt base pipewire pipewire-alsa pipewire-jack pipewire-pulse libreoffice-fresh plasma-meta krusader ranger btop neovim thunderbird firefox geogebra code neovim blueman plasma-nm linux linux-firmware networkmanager freecad blender nano man-db e2fsprogs ntfs-3g dosfstools grub efibootmgr gnome gdm
 genfstab -U /mnt >> /mnt/etc/fstab
-arch-chroot /mnt << EOF
+echo "entering chroot"
+arch-chroot /mnt /bin/bash -i << EOF
+#!/bin/bash
 ln -sf /usr/share/zoneinfo/Europe/Istanbul /etc/localtime
 hwclock --systohc >&/dev/null
 echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 locale-gen >&/dev/null
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf
 echo "KEYMAP=trq" >> /etc/vconsole.conf
-echo "What should be the name of the computer?"
-read -r HOSTNAME
-echo "$HOSTNAME" >> /etc/hostname
-passwd
+echo "edarch-pc" >> /etc/hostname
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 cat >> /etc/default/grub << EOR
 GRUB_DEFAULT=0
@@ -80,11 +80,29 @@ GRUB_GFXPAYLOAD_LINUX=keep
 GRUB_DISABLE_RECOVERY=true
 GRUB_DISABLE_OS_PROBER=false
 EOR
-pacman -Syu gnome plasma-desktop >&/dev/null
-pacman -Syu gnome plasma-desktop >&/dev/null
+grub-mkconfig -o /boot/grub/grub.cfg
+echo "root:root" | chpasswd
+ln -s /usr/lib/systemd/system/gdm.service /etc/systemd/system/display-manager.service
+ln -s /usr/lib/systemd/system/NetworkManager.service /etc/systemd/system/multi-user.target.wants/NetworkManager.service
+ln -s /usr/lib/systemd/system/NetworkManager-dispatcher.service /etc/systemd/system/dbus-org.freedesktop.nm-dispatcher.service
+ln -s /usr/lib/systemd/system/NetworkManager-wait-online.service /etc/systemd/system/network-online.target.wants/NetworkManager-wait-online.service
+mkdir -p /home/user
+useradd -d /home/user -G users,video,audio,wheel user
+echo "user:user" | chpasswd
+chown user /home/user
+mkdir /home/user/Downloads
+mkdir /home/user/Documents
+mkdir /home/user/Desktop
+mkdir /home/user/Videos
+mkdir /home/user/Pictures
+mkdir /home/user/Music
+chown user /home/user/Downloads
+chown user /home/user/Documents
+chown user /home/user/Desktop
+chown user /home/user/Videos
+chown user /home/user/Pictures
+chown user /home/user/Music
 EOF
-echo "Unmount bootable USB and input OK"
-read -r YESNO
 echo "rebooting"
 sleep 3
 umount -R /mnt
