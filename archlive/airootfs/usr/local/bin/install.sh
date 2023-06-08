d278d13901600dc4073206a1172b835249ba0b75
ahmetemregurdal <ahmetemregurdal2008@gmail.com>
2023-06-08 15:12:11 +0300

    added arduino IDE, android studio and scid with stockfish chess engine to installer

--- archlive/airootfs/usr/local/bin/install.sh
+++ archlive/airootfs/usr/local/bin/install.sh
@@ -53,7 +53,7 @@ if [[ ${SWAPNAME} != "n" ]]; then
 	swapon "$SWAP"
 fi
 echo "bootstraping"
-pacstrap -K /mnt base linux linux-firmware networkmanager nano man-db e2fsprogs ntfs-3g dosfstools grub efibootmgr xf86-video-openchrome xf86-video-nouveau xf86-video-sisusb xf86-video-amdgpu xf86-video-vmware xf86-video-voodoo xf86-video-fbdev xf86-video-intel xf86-video-dummy xf86-video-vesa xf86-video-qxl xf86-video-ati intel-ucode amd-ucode os-prober
+pacstrap -K /mnt base linux linux-firmware networkmanager nano man-db e2fsprogs ntfs-3g dosfstools grub efibootmgr intel-ucode amd-ucode os-prober
 genfstab -U /mnt >> /mnt/etc/fstab
 echo "entering chroot"
 arch-chroot /mnt /bin/bash -i << EOF
@@ -88,8 +88,12 @@ ln -s /usr/lib/systemd/system/NetworkManager.service /etc/systemd/system/multi-u
 ln -s /usr/lib/systemd/system/NetworkManager-dispatcher.service /etc/systemd/system/dbus-org.freedesktop.nm-dispatcher.service
 ln -s /usr/lib/systemd/system/NetworkManager-wait-online.service /etc/systemd/system/network-online.target.wants/NetworkManager-wait-online.service
 mkdir -p /home/user
-useradd -d /home/user -G users,video,audio,wheel user
+mkdir -p /home/admin
+useradd -d /home/admin -G users,video,audio,wheel admin
+useradd -d /home/user -G users,video,audio user
 echo "user:user" | chpasswd
+echo "admin:admin" | chpasswd
+chown admin /home/admin
 chown user /home/user
 mkdir /home/user/Downloads
 mkdir /home/user/Documents
@@ -103,10 +107,10 @@ chown user /home/user/Desktop
 chown user /home/user/Videos
 chown user /home/user/Pictures
 chown user /home/user/Music
-pacman -S --noconfirm libreoffice-fresh krusader ranger btop neovim thunderbird firefox geogebra neovim blueman freecad blender gimp krita git base-devel sudo esptool hicolor-icon-theme mypy python-asttokens python-docutils python-jedi python-pillow python-pip python-ptyprocess python-pylint python-send2trash python-setuptools python-wheel python-build python-installer tk python-numpy python-pygame python-cached-property python python-django python-pytest python-humanize python-littleutils python-sqlalchemy python-setuptools-scm python-beautifulsoup4 python-flask python-markupsafe python-pandas geckodriver python-requests python-certifi python-debugpy python-importlib-metadata python-inflection python-multidict python-urllib3 alsa-lib gcc-libs glibc gnupg gtk3 libnotify libsecret libxkbfile libxss lsof nss shared-mime-info glib2 libdbusmenu-glib gnome-keyring cpio openssl-1.1 libappindicator-gtk3 clang make sh patch dbus-glib libldap libxml2 polkit gnome-common gobject-introspection gtk-doc intltool nemo go python-exceptiongroup python-trio python-wsproto
+pacman -S --noconfirm libreoffice-fresh krusader ranger btop neovim thunderbird firefox geogebra neovim blueman freecad blender gimp krita git base-devel sudo esptool hicolor-icon-theme mypy python-asttokens python-docutils python-jedi python-pillow python-pip python-ptyprocess python-pylint python-send2trash python-setuptools python-wheel python-build python-installer tk python-numpy python-pygame python-cached-property python python-django python-pytest python-humanize python-littleutils python-sqlalchemy python-setuptools-scm python-beautifulsoup4 python-flask python-markupsafe python-pandas geckodriver python-requests python-certifi python-debugpy python-importlib-metadata python-inflection python-multidict python-urllib3 alsa-lib gcc-libs glibc gnupg gtk3 libnotify libsecret libxkbfile libxss lsof nss shared-mime-info glib2 libdbusmenu-glib gnome-keyring cpio openssl-1.1 libappindicator-gtk3 clang make sh patch dbus-glib libldap libxml2 polkit gnome-common gobject-introspection gtk-doc intltool nemo go python-exceptiongroup python-trio python-wsproto xorg-drivers xorg xorg-apps appstream-glib archlinux-appstream-data desktop-file-utils gnutls json-glib libhandy vte3 asciidoc gettext itstool meson ninja vala xorgproto lxsession polkit-gnome libsoup3 libusb libusb-compat python-pyserial usbutils freetype2 libxrender libxtst which gtk2 libglvnd libpng zlib libxft tcl gumbo-parser libjpeg-turbo libtiff tcllib
 git clone https://aur.archlinux.org/yay-bin.git
-chown user yay-bin
-su user << EOR
+chown admin yay-bin
+su admin << EOR
 cd yay-bin
 makepkg
 cd ..
@@ -115,12 +119,12 @@ pacman -U --noconfirm yay-bin/yay-bin*.tar.zst
 rm -rf yay-bin
 cat >> /etc/sudoers << EOR
 root ALL=(ALL:ALL) ALL
-ALL ALL=(ALL:ALL) ALL
+%wheel ALL=(ALL:ALL) NOPASSWD: ALL
 @includedir /etc/sudoers.d
 EOR
 git clone https://aur.archlinux.org/python-trio-websocket.git
-chown user python-trio-websocket
-su user << EOR
+chown admin python-trio-websocket
+su admin << EOR
 cd python-trio-websocket
 makepkg
 cd ..
@@ -128,8 +132,8 @@ EOR
 pacman -U --noconfirm python-trio-websocket/python-trio-websocket*.tar.zst
 rm -rf python-trio-websocket
 git clone https://aur.archlinux.org/python-outdated.git
-chown user python-outdated
-su user << EOR
+chown admin python-outdated
+su admin << EOR
 cd python-outdated
 makepkg
 cd ..
@@ -137,8 +141,8 @@ EOR
 pacman -U --noconfirm python-outdated/python-outdated*.tar.zst
 rm -rf python-outdated
 git clone https://aur.archlinux.org/python-cheap_repr.git
-chown user python-cheap_repr
-su user << EOR
+chown admin python-cheap_repr
+su admin << EOR
 cd python-cheap_repr
 makepkg
 cd ..
@@ -146,8 +150,8 @@ EOR
 pacman -U --noconfirm python-cheap_repr/python-cheap_repr*.tar.zst
 rm -rf python-cheap_repr
 git clone https://aur.archlinux.org/python-flask-humanize.git
-chown user python-flask-humanize
-su user << EOR
+chown admin python-flask-humanize
+su admin << EOR
 cd python-flask-humanize
 makepkg
 cd ..
@@ -155,8 +159,8 @@ EOR
 pacman -U --noconfirm python-flask-humanize/python-flask-humanize*.tar.zst
 rm -rf python-flask-humanize
 git clone https://aur.archlinux.org/python-pgzero.git
-chown user python-pgzero
-su user << EOR
+chown admin python-pgzero
+su admin << EOR
 cd python-pgzero
 makepkg
 cd ..
@@ -164,8 +168,8 @@ EOR
 pacman -U --noconfirm python-pgzero/python-pgzero*.tar.zst
 rm -rf python-pgzero
 git clone https://aur.archlinux.org/python-selenium.git
-chown user python-selenium
-su user << EOR
+chown admin python-selenium
+su admin << EOR
 cd python-selenium
 makepkg
 cd ..
@@ -173,8 +177,8 @@ EOR
 pacman -U --noconfirm python-selenium/python-selenium*.tar.zst
 rm -rf python-selenium
 git clone https://aur.archlinux.org/python-birdseye.git
-chown user python-birdseye
-su user << EOR
+chown admin python-birdseye
+su admin << EOR
 cd python-birdseye
 makepkg
 cd ..
@@ -182,8 +186,8 @@ EOR
 pacman -U --noconfirm python-birdseye/python-birdseye*.tar.zst
 rm -rf python-birdseye
 git clone https://aur.archlinux.org/thonny.git
-chown user thonny
-su user << EOR
+chown admin thonny
+su admin << EOR
 cd thonny
 makepkg
 cd ..
@@ -191,8 +195,8 @@ EOR
 pacman -U --noconfirm thonny/thonny*.tar.zst
 rm -rf thonny
 git clone https://aur.archlinux.org/visual-studio-code-bin.git
-chown user visual-studio-code-bin
-su user << EOR
+chown admin visual-studio-code-bin
+su admin << EOR
 cd visual-studio-code-bin
 makepkg
 cd ..
@@ -200,8 +204,8 @@ EOR
 pacman -U --noconfirm visual-studio-code-bin/visual-studio-code-bin*.tar.zst
 rm -rf visual-studio-code-bin
 git clone https://aur.archlinux.org/libicu50.git
-chown user libicu50
-su user << EOR
+chown admin libicu50
+su admin << EOR
 cd libicu50
 makepkg
 cd ..
@@ -209,8 +213,8 @@ EOR
 pacman -U --noconfirm libicu50/libicu50*.tar.zst
 rm -rf libicu50
 git clone https://aur.archlinux.org/gconf.git
-chown user gconf
-su user << EOR
+chown admin gconf
+su admin << EOR
 cd gconf
 makepkg
 cd ..
@@ -218,8 +222,8 @@ EOR
 pacman -U --noconfirm gconf/gconf*.tar.zst
 rm -rf gconf
 git clone https://aur.archlinux.org/icu70.git
-chown user icu70
-su user << EOR
+chown admin icu70
+su admin << EOR
 cd icu70
 makepkg
 cd ..
@@ -227,8 +231,8 @@ EOR
 pacman -U --noconfirm icu70/icu70*.tar.zst
 rm -rf icu70
 git clone https://aur.archlinux.org/icu69-bin.git
-chown user icu69-bin
-su user << EOR
+chown admin icu69-bin
+su admin << EOR
 cd icu69-bin
 makepkg
 cd ..
@@ -236,16 +240,108 @@ EOR
 pacman -U --noconfirm icu69-bin/icu69-bin*.tar.zst
 rm -rf icu69-bin
 git clone https://aur.archlinux.org/unityhub.git
-chown user unityhub
-su user << EOR
+chown admin unityhub
+su admin << EOR
 cd unityhub
 makepkg
 cd ..
 EOR
 pacman -U --noconfirm unityhub/unityhub*.tar.zst
 rm -rf unityhub
+git clone https://aur.archlinux.org/libpamac-aur.git
+chown admin libpamac-aur
+su admin << EOR
+cd libpamac-aur
+makepkg
+cd ..
+EOR
+pacman -U --noconfirm libpamac-aur/libpamac-aur*.tar.zst
+rm -rf libpamac-aur
+git clone https://aur.archlinux.org/pamac-aur.git
+chown admin pamac-aur
+su admin << EOR
+cd pamac-aur
+makepkg
+cd ..
+EOR
+pacman -U --noconfirm pamac-aur/pamac-aur*.tar.zst
+rm -rf pamac-aur
+git clone https://aur.archlinux.org/arduino-ide-bin.git
+chown admin arduino-ide-bin
+su admin << EOR
+cd arduino-ide-bin
+makepkg
+cd ..
+EOR
+pacman -U --noconfirm arduino-ide-bin/arduino-ide-bin*.tar.zst
+rm -rf arduino-ide-bin
+git clone https://aur.archlinux.org/ncurses5-compat-libs.git
+chown admin ncurses5-compat-libs
+su admin << EOR
+cd ncurses5-compat-libs
+makepkg
+cd ..
+EOR
+pacman -U --noconfirm ncurses5-compat-libs/ncurses5-compat-libs*.tar.zst
+rm -rf ncurses5-compat-libs
+git clone https://aur.archlinux.org/android-studio.git
+chown admin android-studio
+su admin << EOR
+cd android-studio
+makepkg
+cd ..
+EOR
+pacman -U --noconfirm android-studio/android-studio*.tar.zst
+rm -rf android-studio
+git clone https://aur.archlinux.org/sublime-text-4.git
+chown admin sublime-text-4
+su admin << EOR
+cd sublime-text-4
+makepkg
+cd ..
+EOR
+pacman -U --noconfirm sublime-text-4/sublime-text-4*.tar.zst
+rm -rf sublime-text-4
+git clone https://aur.archlinux.org/snack.git
+chown admin snack
+su admin << EOR
+cd snack
+makepkg
+cd ..
+EOR
+pacman -U --noconfirm snack/snack*.tar.zst
+rm -rf snack
+git clone https://aur.archlinux.org/tkimg.git
+chown admin tkimg
+su admin << EOR
+cd tkimg
+makepkg
+cd ..
+EOR
+pacman -U --noconfirm tkimg/tkimg*.tar.zst
+rm -rf tkimg
+git clone https://aur.archlinux.org/scid.git
+chown admin scid
+su admin << EOR
+cd scid
+makepkg
+cd ..
+EOR
+pacman -U --noconfirm scid/scid*.tar.zst
+rm -rf scid
+git clone https://aur.archlinux.org/stockfish.git
+chown admin stockfish
+su admin << EOR
+cd stockfish
+makepkg
+cd ..
+EOR
+pacman -U --noconfirm stockfish/stockfish*.tar.zst
+rm -rf stockfish
+pacman -Rsu gnome-software
 EOF
 echo "rebooting"
 sleep 3
 umount -R /mnt
 reboot
+
